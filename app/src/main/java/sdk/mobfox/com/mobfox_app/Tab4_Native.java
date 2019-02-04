aa61c7079e2fd10a987c604b30f2176dcd7d8369
Shahaf <shahaf.sh@matomy.com>
2019-02-04 16:11:12 +0200

    sdk & adapters version update

---
+++ app/src/main/java/sdk/mobfox/com/mobfox_app/Tab4_Native.java
@@ -0,0 +1,13 @@
+package sdk.mobfox.com.mobfox_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Tab4_Native extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tab4_native);
+    }
+}
